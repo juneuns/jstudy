554c8abcdcdc67f017d7eb566624c47e7a611c26
class02 <class02@192.168.0.21>
2020-07-27 20:12:13 +0900

    day08 last commit

--- class02/src/day08/Ex04.java
+++ class02/src/day08/Ex04.java
@@ -8,7 +8,7 @@ package day08;
 		 	
 		 	0/5		- 반지름이 5인 원
 		 	3/5/10	- 밑변이 5이고 높이가 10인 삼각형
-		 	4/3/4	- 가로가 3이고 세로가 4인 사각형
+		 	4/3/4	- 가로가 3이고 세로가  4인 사각형
 		 	
 		의 형식으로 입력 받은 도형의 넓이를 계산해서 출력하세요.
 		
@@ -22,14 +22,36 @@ package day08;
 			뒤에서 부터 찾을 문자를 검색해서 해당 위치를 반환해준다.
 		 	
  */
+
+import javax.swing.*;
 public class Ex04 {
 
 	public static void main(String[] args) {
-		String str = "aebedefg";
-		int idx = str.lastIndexOf('e');
-		System.out.println(idx);
-		idx = str.indexOf('e', str.indexOf('e') + 1);
-		System.out.println(idx);
+		String str = JOptionPane.showInputDialog("도형/숫자1[/숫자2] 의 형식으로 입력해주세요!");
+		String[] sArr = new String[3];
+		
+		// 실제데이터 형태의 배열만들고
+		int[] noArr = new int[3];
+		
+		sArr[0] = str.substring(0, str.indexOf('/'));
+		/*
+		if(!sArr[0].equals("0")) {
+			// 사각형, 삼각형인 경우
+			sArr[1] = str.substring(str.indexOf('/') + 1, str.lastIndexOf('/'));
+		} else {
+			// 원인 경우
+			sArr[1] = str.substring(str.indexOf('/') + 1);
+		}
+		*/
+		sArr[1] = sArr[0].equals("0") ? str.substring(str.indexOf('/') + 1) : str.substring(str.indexOf('/') + 1, str.lastIndexOf('/'));
+		sArr[2] = sArr[0].equals("0") ? "0" : str.substring(str.lastIndexOf('/') + 1);
+		
+		for(int i = 0 ; i < noArr.length ; i++ ) {
+			noArr[i] = Integer.parseInt(sArr[i]);
+		}
+		
+		// 데이터 꺼내서 계산해서 출력한다.
+		
 	}
 
 }
