418f58380903167fda04a9b33d8c52ed5fc63b64
class02 <class02@192.168.0.21>
2020-08-10 16:15:56 +0900

    day18 3rd commit

---
+++ class02/src/day18/Ex02.java
@@ -0,0 +1,98 @@
+package day18;
+
+import javax.swing.JOptionPane;
+
+/*
+	삼각형 클래스를 만드는데
+	삼각형의 밑변이 같으면 같은 삼각형이 되도록 정의하고
+	밑변 높이를 1 ~ 20 사이의 정수로 랜덤하게 만들어서
+	삼각형 두개를 만들고
+	만들어진 삼각형들을 비교해서
+	같은지 여부를 출력하세요. -- equals() 오버라이드
+	
+	단, 3보다 작은 숫자가 발생하면	- 사용자정의 예외 만들기...
+	사용자 정의 예외를 만들어서 처리하세요.
+	15보다 큰 숫자는 NumberFormatException 으로 처리하세요.
+	+
+	클래스를 출력하면 클래스의 내용이 출력되게  - toString() 오버라이드
+	함수를 오버라이드하세요.
+ */
+public class Ex02 {
+
+	public Ex02() {
+		int[] size = new int[4];
+		for(int i = 0 ; i < 4 ; i++ ) {
+			int no = (int)(Math.random()*20 + 1);
+			try {
+				if(no < 3) {
+					// 이 경우도 JVM에게 강제로 예외 처리하라고 알려줘야 하는 경우에 해당한다.
+					// 사용자 정의 예외로....
+					throw new Excpt02();
+				} else if(no > 15) {
+					// 이 경우는 사용할 수 없는 숫자이므로 예외를 강제로 발생시켜준다.
+					// 일반적인 경우에는 예외가 전혀아니므로 JVM 따로 처리할 수 없다.
+					// 다만 이 프로그램의 경우 NumberFormatException 으로 처리하기로 했으니
+					// JVM에게 강제로 처리하라고 알려준다.
+					throw new NumberFormatException(); // 
+				}
+				
+				size[i] = no;
+			} catch(NumberFormatException e) {
+				i--;
+				e.printStackTrace();
+			} catch(Excpt02 e) {
+				i--;
+				e.printStackTrace();
+			}
+		}
+		
+		// 삼각형에 필요한 사이즈는 정해졌으니 이제 삼각형을 만들고 비교해본다.
+		Samgak s1 = new Samgak(size[0], size[1]);
+		Samgak s2 = new Samgak(size[2], size[3]);
+		
+		String msg = s1.equals(s2) ? "같은 " : "다른 ";
+		JOptionPane.showMessageDialog(null, "삼각형 s1과 s2 는 " + msg + "삼각형입니다.\ns1.width : " + s1.width + "\ns2.width : " + s2.width);
+	}
+
+	public static void main(String[] args) {
+		new Ex02();
+	}
+
+}
+
+class Samgak {
+	int width, height;
+	double area;
+	public Samgak() {}
+	public Samgak(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+	}
+	
+	// 면적 구해주는 함수
+	public void setArea() {
+		area = width * height / 2.0;
+	}
+	
+	// 밑변이 같으면 같은 삼각형으로 되게 equals() 오버라이드
+	public boolean equals(Object o) {
+		/*
+			boolean bool = false;
+			Samgak s1 = (Samgak) o;
+			if(this.width == s1.width){
+				bool = true;
+			}
+			return bool;
+		 */
+		return this.width == ((Samgak) o).width;
+	}
+}
+
+class Excpt02 extends Exception {
+	public String toString() {
+		String s = getClass().getName();
+        String message = "15보다 큰 숫자입니다.";
+        return (message != null) ? (s + ": " + message) : s;
+	}
+}
---
+++ class02/src/day18/Ex03.java
@@ -0,0 +1,81 @@
+package day18;
+
+import javax.swing.JOptionPane;
+
+/*
+	학생의 
+		이름, java, oracle, web, jsp, total
+	성적을 관리할 클래스를 작성하세요.
+	
+	1.  toString() 오버라이드
+		단, 클래스를 출력하면
+			"???? 학생의 성적 : java - xxx, oracle - xxx, web - xxx, jsp - xxx, total - xxx"
+		의 형식으로 출력되게 하세요.
+	
+	2. 	equals() 오버라이드
+		학생이름이 같으면 같은 학생으로 처리되게 하세요.
+ */
+public class Ex03 {
+
+	public Ex03() {
+		// 두 학생의 데이터를 입력해 보자. 반복해서 처리할 것이므로 배열로 만들어서 처리하자.
+		Stud[] stud = new Stud[2];
+		
+		for(int i = 0 ; i < stud.length ; i++ ) {
+			String name = JOptionPane.showInputDialog("이름을 입력하세요!");
+			// 같은 이름이 발생하면 예외가 발생하게 처리
+			try {
+				if(i != 0 && name.equals(stud[i - 1].name)) {
+					throw new Exception();
+				}
+			} catch(Exception e) {
+				i--;
+				JOptionPane.showMessageDialog(null, "같은 학생이 입력되었습니다.");
+			}
+			
+			// 과목점수를 만든다.
+			int[] score = new int[4];
+			for(int j = 0 ; j < 4 ; j++ ) {
+				score[j] = (int)(Math.random()*41 + 60);
+			}
+			stud[i] = new Stud(name, score[0], score[1], score[2], score[3]);
+		}
+		
+		// 꺼내서 확인해보자.
+		String msg = stud[0].name.equals(stud[1].name) ? "같은 " : "다른 ";
+		JOptionPane.showMessageDialog(null, "두 학생은 " + msg + "학생입니다.\n" + stud[0] + "\n" + stud[1]);
+	}
+
+	public static void main(String[] args) {
+		new Ex03();
+	}
+
+}
+
+class Stud {
+	String name;
+	int java, oracle, web, jsp, total;
+	
+	public Stud() {}
+	public Stud(String name, int java, int oracle, int web, int jsp) {
+		this.name = name;
+		this.java = java;
+		this.oracle = oracle;
+		this.web = web;
+		this.jsp = jsp;
+		setTotal();
+	}
+	
+	// 총점 계산해주는 함수
+	public void setTotal() {
+		total = java + oracle + web + jsp;
+	}
+	
+	public String toString() {
+		return name + " 학생의 성적 : java - " + java + ", oracle - " + oracle + ", web - " + web + ", jsp - " + jsp + ", total - " + total;
+	}
+	
+	public boolean equals(Object o) {
+		return name.equals(((Stud) o).name); // 문자열 비교는 == 로 절대 비교하지 말것.... 반드시 equals() 함수로 처리할 것...
+	}
+}
