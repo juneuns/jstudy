36d9be99a363400cc4da39c821179c025072bde8
class02 <class02@192.168.0.21>
2020-08-13 15:25:26 +0900

    day21 5th commit

---
+++ class02/src/day21/Test06.java
@@ -0,0 +1,64 @@
+package day21;
+
+import java.util.ArrayList;
+
+/*
+	랜덤하게 10 ~ 50 사이의 반지름을 가지는 Won을
+	ArrayList에 10개를 채우고
+	출력하고
+	정렬하고
+	출력하세요.
+ */
+
+import java.util.*;
+
+public class Test06 {
+
+	public Test06() {
+		// ArrayList 만들고
+		ArrayList list = new ArrayList();
+		// 10번 반복해서 데이터 채우고
+		for(int i = 0 ; i < 10 ; i++ ) {
+			Won w = getWon();
+			list.add(w);
+		}
+		
+		// 출력하고
+		System.out.println("1. 정렬 전");
+		for(Object o : list) {
+			System.out.println(o);
+		}
+		// 정렬하고
+		Collections.sort(list);
+		// 출력하고
+		System.out.println("1. 정렬 후");
+		for(Object o : list) {
+			System.out.println(o);
+		}
+		
+		// 내림차순 정렬하고
+		Collections.reverse(list);
+		// 출력하고
+		System.out.println("1. 내림차순 정렬 후");
+		for(Object o : list) {
+			System.out.println(o);
+		}
+	}
+	
+	// 랜덤하게 반지름을 만들어서 Won에 데이터 셋팅하고 넘겨주는 함수
+	public Won getWon() {
+		Won won = new Won();
+		// 데이터 채우고
+		int rad = (int)(Math.random()*41 + 10);
+		won.setRad(rad);
+		won.setArround();
+		won.setArea();
+		
+		return won;
+	}
+
+	public static void main(String[] args) {
+		new Test06();
+	}
+
+}
---
+++ class02/src/day21/Won.java
@@ -0,0 +1,59 @@
+package day21;
+
+import java.text.*;
+
+public class Won implements Comparable {
+	private final int ISSTR = 1000;
+	private int rad; // 반지름 기억할 변수
+	private double arround; // 둘레 기억할 변수
+	private double area; // 넓이 기억할 변수
+	// 접근지정자가 이 클래스내에서만 접근하도록 되어있으므로 
+	// 데이터를 꺼내고 기억시키는 기능의 함수를 만들어 두자.
+	public int getRad() {
+		return rad;
+	}
+	public void setRad(int rad) {
+		this.rad = rad;
+	}
+	public String getArround(int code) {
+		DecimalFormat form = new DecimalFormat("###############.00");
+		String str = form.format(arround);
+		return str;
+	}
+	public double getArround() {
+		return arround;
+	}
+	public void setArround() {
+		this.arround = 2 * rad * Math.PI;
+	}
+	public void setArround(double arround) {
+		this.arround = arround;
+	}
+	public String getArea(int code) {
+		DecimalFormat form = new DecimalFormat("###############.00");
+		String str = form.format(area);
+		return str;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void setArea() {
+		this.area = rad * rad * Math.PI;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	@Override
+	public String toString() {
+		// 우리가 만드는 모든 클래스는 Object 클래스를 상속 받아서 만드는 클래스가 되므로
+		// 당연히 Object 가지고 있는 멤버는 모두 가지고 있게 된다.
+		return "Circle : rad = " + rad + ", arround = " + getArround(ISSTR) + ", area = " + getArea(ISSTR) ;
+	}
+	@Override
+	public int compareTo(Object o) {
+		Won won = (Won) o;
+		double area1 = won.getArea();
+		return (area - area1) < 0 ? -1 : 1;
+	}
+	
+}
